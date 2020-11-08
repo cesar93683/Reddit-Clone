53c642ed81a89fb3af4711ae6f9fdc701b53756e
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2020-11-08 14:06:19 -0800

    add sign up, log in, home pages. add router

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+import NavBar from "./Navbar/Navbar";
+import SignUp from "./SignUp/SignUp";
+import LogIn from "./LogIn/LogIn";
+import Home from "./Home/Home";
+
+
+const App = () => {
+  return <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/signup" exact>
+          <SignUp />
+        </Route>
+        <Route path="/login" exact>
+          <LogIn />
+        </Route>
+        
+        <Redirect to="/" />
+      </Switch>
+  </Router>
+};
+
+export default App;
