cf99c9842167fa6980d5abb7cddaf18ad89114d7
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2020-11-22 09:04:07 -0800

    finish signUp mutation

--- graphql-auth/src/types/Mutation.ts
+++ graphql-auth/src/types/Mutation.ts
@@ -13,21 +13,48 @@ export const Mutation = mutationType({
         password: stringArg({ nullable: false }),
       },
       resolve: async (_parent, { username, email, password }, ctx) => {
+        let existingUser
         try {
-          const hashedPassword = await hash(password, 10)
-          const user = await ctx.prisma.user.create({
+          existingUser = await ctx.prisma.user.findOne({ where: { email } })
+        } catch (err) {
+          throw new Error('Signing up failed, please try again later.')
+        }
+
+        if (existingUser) {
+          throw new Error('User exists already, please login instead.')
+        }
+
+        let hashedPassword
+        try {
+          hashedPassword = await hash(password, 12)
+        } catch (err) {
+          throw new Error('Could not create user, please try again.')
+        }
+
+        let user
+
+        try {
+          user = await ctx.prisma.user.create({
             data: {
               username,
               email,
               password: hashedPassword,
             },
           })
-          return {
-            token: sign({ userId: user.id }, APP_SECRET),
-            user,
-          }
         } catch (err) {
-          throw new Error('An error occured')
+          throw new Error('Could not create user, please try again.')
+        }
+
+        let token
+        try {
+          token = sign({ userId: user.id }, APP_SECRET)
+        } catch (err) {
+          throw new Error('Could not create user, please try again.')
+        }
+
+        return {
+          token,
+          user,
         }
       },
     })
@@ -39,25 +66,21 @@ export const Mutation = mutationType({
         password: stringArg({ nullable: false }),
       },
       resolve: async (_parent, { email, password }, ctx) => {
-        try {
-          const user = await ctx.prisma.user.findOne({
-            where: {
-              email,
-            },
-          })
-          if (!user) {
-            throw new Error(`No user found for email: ${email}`)
-          }
-          const passwordValid = await compare(password, user.password)
-          if (!passwordValid) {
-            throw new Error('Invalid password')
-          }
-          return {
-            token: sign({ userId: user.id }, APP_SECRET),
-            user,
-          }
-        } catch (err) {
-          throw new Error('An error occured')
+        const user = await ctx.prisma.user.findOne({
+          where: {
+            email,
+          },
+        })
+        if (!user) {
+          throw new Error(`No user found for email: ${email}`)
+        }
+        const passwordValid = await compare(password, user.password)
+        if (!passwordValid) {
+          throw new Error('Invalid password')
+        }
+        return {
+          token: sign({ userId: user.id }, APP_SECRET),
+          user,
         }
       },
     })
@@ -69,19 +92,35 @@ export const Mutation = mutationType({
         content: stringArg({ nullable: false }),
       },
       resolve: (parent, { title, content }, ctx) => {
-        try {
-          const userId = getUserId(ctx)
-          if (!userId) throw new Error('Could not authenticate user.')
-          return ctx.prisma.post.create({
-            data: {
-              title,
-              content,
-              author: { connect: { id: Number(userId) } },
-            },
-          })
-        } catch (err) {
-          throw new Error('An error occured')
-        }
+        const userId = getUserId(ctx)
+        if (!userId) throw new Error('Could not authenticate user.')
+        return ctx.prisma.post.create({
+          data: {
+            title,
+            content,
+            author: { connect: { id: Number(userId) } },
+          },
+        })
+      },
+    })
+
+    t.field('editPost', {
+      type: 'Post',
+      args: {
+        content: stringArg({ nullable: false }),
+        id: intArg({ nullable: false }),
+      },
+      resolve: (parent, { content, id }, ctx) => {
+        const userId = getUserId(ctx)
+        if (!userId) throw new Error('Could not authenticate user.')
+        return ctx.prisma.post.update({
+          data: {
+            content,
+          },
+          where: {
+            id,
+          },
+        })
       },
     })
 
@@ -90,15 +129,11 @@ export const Mutation = mutationType({
       nullable: true,
       args: { id: intArg({ nullable: false }) },
       resolve: (parent, { id }, ctx) => {
-        try {
-          return ctx.prisma.post.delete({
-            where: {
-              id,
-            },
-          })
-        } catch (err) {
-          throw new Error('An error occured')
-        }
+        return ctx.prisma.post.delete({
+          where: {
+            id,
+          },
+        })
       },
     })
   },
