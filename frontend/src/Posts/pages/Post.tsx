a83867bc24953d22ff4f6423ff7775c88a5c2680
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2020-11-26 10:10:12 -0800

    fix no comments error showing after adding new comment

--- frontend/src/Posts/pages/Post.tsx
+++ frontend/src/Posts/pages/Post.tsx
@@ -67,9 +67,10 @@ const PostItem = () => {
         />
         <div className="bg-dark-gray p-3">
           {auth.isLoggedIn && <CommentForm onSubmit={onSubmitComment} />}
-          {data.getPostById.comments.length === 0 && (
-            <h2 className="text-light">No Comments</h2>
-          )}
+          {data.getPostById.comments.length === 0 &&
+            newComments.length == 0 && (
+              <h2 className="text-light">No Comments</h2>
+            )}
           {newComments.map((comment: IComment) => (
             <Comment
               key={comment.id}
