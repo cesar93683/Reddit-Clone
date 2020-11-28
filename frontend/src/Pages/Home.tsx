8c3f5351f03aa3ceb6f4ee6c378e90e412d44ebd
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2020-11-27 16:33:14 -0800

    can sort posts

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 
 import IPost from "../utils/interfaces/IPost";
 import { useQuery } from "@apollo/client";
@@ -6,10 +6,24 @@ import { POSTS_QUERY } from "../GraphQL/Query";
 
 import LoadingSpinner from "../components/LoadingSpinner";
 import CustomCard from "../components/CustomCard";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 
 const Home = () => {
   const currentDate = Date.now();
   const { loading, data, error } = useQuery(POSTS_QUERY);
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [topActive, setTopActive] = useState(false);
+  const [newActive, setNewActive] = useState(true);
+
+  useMemo(() => {
+    if (data) {
+      setPosts(
+        [...data.posts].sort(
+          (a: IPost, b: IPost) => b.dateCreated - a.dateCreated
+        )
+      );
+    }
+  }, [data]);
 
   if (loading) {
     return <LoadingSpinner />;
@@ -19,13 +33,35 @@ const Home = () => {
     return <h1>An error occured.</h1>;
   }
 
-  if (data.posts.length === 0) {
+  if (posts.length === 0) {
     return <h1>No Posts</h1>;
   }
 
+  console.log(posts);
+
+  const sortByVotes = () => {
+    setPosts([...posts].sort((a, b) => b.numVotes - a.numVotes));
+    setTopActive(true);
+    setNewActive(false);
+  };
+
+  const sortByNew = () => {
+    setPosts([...posts].sort((a, b) => b.dateCreated - a.dateCreated));
+    setTopActive(false);
+    setNewActive(true);
+  };
+
   return (
     <div>
-      {data.posts.map((post: IPost) => (
+      <DropdownButton title="Sort By">
+        <Dropdown.Item onClick={sortByVotes} active={topActive}>
+          Top
+        </Dropdown.Item>
+        <Dropdown.Item onClick={sortByNew} active={newActive}>
+          New
+        </Dropdown.Item>
+      </DropdownButton>
+      {posts.map((post: IPost) => (
         <CustomCard
           className="my-2"
           key={post.id}
