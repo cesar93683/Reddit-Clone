02a2056969319499f3c3a6e0a569e87b697469a8
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2020-11-30 19:37:47 -0800

    rename

--- frontend/src/components/tests/CommentForm.test.tsx
+++ frontend/src/components/tests/CommentForm.test.tsx
@@ -3,13 +3,13 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import CommentForm from "../CommentForm";
 
-test("error is shown", async () => {
+test("error is shown when submitting empty comment", async () => {
   render(<CommentForm onSubmit={() => {}} />);
   fireEvent.click(screen.getByText("Comment"));
   expect(screen.getByText("Please Enter A Comment.")).toBeInTheDocument();
 });
 
-test("error is shown", async () => {
+test("calls 'onSubmit' when form submitted", async () => {
   const onSubmit = jest.fn();
   render(<CommentForm onSubmit={onSubmit} />);
   const testComment = "test comment";
