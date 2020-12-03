4062880e56504e0903246fcd474a80fc879f2ae8
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2020-12-02 19:44:46 -0800

    add test

---
+++ frontend/src/components/tests/VoteSection.test.tsx
@@ -0,0 +1,45 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { act } from "react-dom/test-utils";
+import { AuthContext } from "../../utils/auth-context";
+import VoteSection from "../VoteSection";
+
+const onUpVote = jest.fn();
+const onDownVote = jest.fn();
+
+describe("<VoteSection />", () => {
+  beforeEach(async () => {
+    act(() => {
+      render(
+        <AuthContext.Provider
+          value={{
+            userId: 1,
+            token: "token",
+            login: (userId: number, token: string) => {},
+            logout: () => {},
+          }}
+        >
+          <VoteSection
+            numVotes={623}
+            currVote={0}
+            onDownVote={onDownVote}
+            onUpVote={onUpVote}
+          />
+        </AuthContext.Provider>
+      );
+    });
+    await act(() => new Promise((resolve) => setTimeout(resolve, 0)));
+  });
+  test("onUpVote is called", () => {
+    act(() => {
+      fireEvent.click(screen.getByText("^"));
+    });
+    expect(onUpVote).toHaveBeenCalled();
+  });
+  test("onDownVote is called", () => {
+    act(() => {
+      fireEvent.click(screen.getByText("v"));
+    });
+    expect(onDownVote).toHaveBeenCalled();
+  });
+});
