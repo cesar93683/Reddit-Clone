89114980b8867ce5c525c9ab08df43989dc6813f
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2020-12-05 15:21:01 -0800

    refactor

--- frontend/src/Pages/User.tsx
+++ frontend/src/Pages/User.tsx
@@ -88,7 +88,7 @@ export default function User() {
     setPosts([...posts].sort((a, b) => b.numVotes - a.numVotes));
     setComments([...comments].sort((a, b) => b.numVotes - a.numVotes));
     setPostsAndComments(
-      [...data.user.comments, ...data.user.posts].sort(
+      [...postsAndComments].sort(
         (a: IPost | IComment, b: IPost | IComment) => b.numVotes - a.numVotes
       )
     );
@@ -100,7 +100,7 @@ export default function User() {
     setPosts([...posts].sort((a, b) => b.dateCreated - a.dateCreated));
     setComments([...comments].sort((a, b) => b.dateCreated - a.dateCreated));
     setPostsAndComments(
-      [...data.user.comments, ...data.user.posts].sort(
+      [...postsAndComments].sort(
         (a: IPost | IComment, b: IPost | IComment) =>
           b.dateCreated - a.dateCreated
       )
