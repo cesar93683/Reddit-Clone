db8ba4b8f8ecd09307da0a0692c14ec89897f063
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2020-12-07 19:19:21 -0800

    fix tests

--- frontend/src/Pages/tests/Home.test.tsx
+++ frontend/src/Pages/tests/Home.test.tsx
@@ -3,7 +3,10 @@ import "@testing-library/jest-dom/extend-expect";
 import { act, fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Home, { POSTS_BY_DATE_CREATED_QUERY } from "../Home";
+import Home, {
+  POSTS_BY_DATE_CREATED_QUERY,
+  POSTS_BY_NUM_VOTES_QUERY,
+} from "../Home";
 
 const mockHistoryPush = jest.fn();
 
@@ -23,44 +26,102 @@ const mocks = [
     result: () => {
       return {
         data: {
-          posts: [
-            {
-              id: 1,
-              title: "title1",
-              numComments: 1,
-              numVotes: 5,
-              dateCreated: 2,
-              dateUpdated: 2,
-              author: {
-                id: 1,
-                username: "username",
+          posts: {
+            posts: [
+              {
+                id: 3,
+                title: "title3",
+                numComments: 1,
+                numVotes: 3,
+                dateCreated: 3,
+                dateUpdated: 3,
+                author: {
+                  id: 1,
+                  username: "username",
+                },
               },
-            },
-            {
-              id: 2,
-              title: "title2",
-              numComments: 1,
-              numVotes: 2,
-              dateCreated: 1,
-              dateUpdated: 1,
-              author: {
+              {
                 id: 1,
-                username: "username",
+                title: "title1",
+                numComments: 1,
+                numVotes: 5,
+                dateCreated: 2,
+                dateUpdated: 2,
+                author: {
+                  id: 1,
+                  username: "username",
+                },
               },
-            },
-            {
-              id: 3,
-              title: "title3",
-              numComments: 1,
-              numVotes: 3,
-              dateCreated: 3,
-              dateUpdated: 3,
-              author: {
+              {
+                id: 2,
+                title: "title2",
+                numComments: 1,
+                numVotes: 2,
+                dateCreated: 1,
+                dateUpdated: 1,
+                author: {
+                  id: 1,
+                  username: "username",
+                },
+              },
+            ],
+            hasMore: false,
+            cursor: 3,
+          },
+        },
+      };
+    },
+  },
+  {
+    request: {
+      query: POSTS_BY_NUM_VOTES_QUERY,
+      variables: {},
+    },
+    result: () => {
+      return {
+        data: {
+          posts: {
+            posts: [
+              {
                 id: 1,
-                username: "username",
+                title: "title1",
+                numComments: 1,
+                numVotes: 5,
+                dateCreated: 2,
+                dateUpdated: 2,
+                author: {
+                  id: 1,
+                  username: "username",
+                },
               },
-            },
-          ],
+              {
+                id: 3,
+                title: "title3",
+                numComments: 1,
+                numVotes: 3,
+                dateCreated: 3,
+                dateUpdated: 3,
+                author: {
+                  id: 1,
+                  username: "username",
+                },
+              },
+              {
+                id: 2,
+                title: "title2",
+                numComments: 1,
+                numVotes: 2,
+                dateCreated: 1,
+                dateUpdated: 1,
+                author: {
+                  id: 1,
+                  username: "username",
+                },
+              },
+            ],
+            hasMore: false,
+            cursor: 2,
+          },
         },
       };
     },
@@ -86,6 +147,7 @@ describe("<Home />", () => {
   test("should be able to sort posts by votes", async () => {
     fireEvent.click(screen.getByText("Sort By"));
     fireEvent.click(screen.getByText("Votes"));
+    await act(() => new Promise((resolve) => setTimeout(resolve, 0)));
     const posts = screen.getAllByTestId("post");
     expect(posts[0]).toHaveTextContent("title1");
     expect(posts[1]).toHaveTextContent("title3");
@@ -93,7 +155,11 @@ describe("<Home />", () => {
   });
   test("should be able to sort posts by new", async () => {
     fireEvent.click(screen.getByText("Sort By"));
+    fireEvent.click(screen.getByText("Votes"));
+    await act(() => new Promise((resolve) => setTimeout(resolve, 0)));
+    fireEvent.click(screen.getByText("Sort By"));
     fireEvent.click(screen.getByText("Date Posted"));
+    await act(() => new Promise((resolve) => setTimeout(resolve, 0)));
     const posts = screen.getAllByTestId("post");
     expect(posts[0]).toHaveTextContent("title3");
     expect(posts[1]).toHaveTextContent("title1");
